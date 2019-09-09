bd91892f3000505317a2fb293de524e02401643c
Rajashekhar <Rajashekhar.Kumbar@causeway.com>
2019-09-09 14:50:04 +0530

    Adding HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class Hello
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello");
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println("no of :"+i);
+		}
+	}
+
+}
+
